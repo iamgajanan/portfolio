18c83d6d9e1b14cfe3fad6c11437a3661adcd4a7
iamgajanan <gajushinde8046@gmail.com>
2021-01-24 23:04:44 +0530

    added bootstrap+font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable,interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() { 
+    // Observable.interval(10000).takeWhile(() => true).subscribe(() => this.function());
+  }
   title = 'portfolio';
+
+  function(){
+
+  }
 }
+
+
